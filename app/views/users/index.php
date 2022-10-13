c3c90bcd92a7bbd1a4fbc1f0de52b692a307920b
aristocaesar <hi@aristoc.space>
2022-10-13 08:44:42 +0700

    add modal on setting users

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -45,11 +45,11 @@
                                         <div class="d-flex flex-column">
                                             <div class="mt-5">
                                                 <h5 class="text-new-blue font-weight-bold">Email</h5>
-                                                <p class="text-dark">hi@aristoc.space - <span class="text-success font-italic">Terverifikasi</span> <a href="#" class="ml-2 text-new-blue underline"><u>Ganti Email</u></a></p>
+                                                <p class="text-dark">hi@aristoc.space - <span class="text-success font-italic">Terverifikasi</span> <a href="#" data-toggle="modal" data-target="#gantiEmail" class="ml-2 text-new-blue underline"><u>Ganti Email</u></a></p>
                                             </div>
                                             <div class="mt-3">
                                                 <h5 class="text-new-blue font-weight-bold">Password</h5>
-                                                <p class="text-dark">Terakhir diperbarui - Senin, 26 September 2022 - 16:26:09<a href="#" class="ml-2 text-new-blue underline"><u>Ganti Password</u></a></p>
+                                                <p class="text-dark">Terakhir diperbarui - Senin, 26 September 2022 - 16:26:09<a href="#" data-toggle="modal" data-target="#gantiPassword" class="ml-2 text-new-blue underline"><u>Ganti Password</u></a></p>
                                             </div>
                                             <div class="mt-3">
                                                 <h5 class="text-new-blue font-weight-bold">Verifikasi</h5>
@@ -70,7 +70,6 @@
         </div>
 </section>
 <!-- Modal Logout -->
-<!-- Modal -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModal" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -86,6 +85,84 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-blue font-weight-bold py-3">Ya, Keluar</button>
             </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Ganti Email -->
+<div class="modal fade" id="gantiEmail" tabindex="-1" role="dialog" aria-labelledby="gantiEmail" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-bold">Ganti Email</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" name="email1" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan email baru">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Konfirmasi Email</label>
+                        <input type="email" name="email2" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Konfirmasi email baru">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <p>Perhatikan</p>
+                        <ul>
+                            <li>Email harus aktif.</li>
+                            <li>Jika link verifikasi tidak tersedia, harap cek pada bagian spam.</li>
+                            <li>Setelah anda mengklik konfirmasi, anda akan otomatis keluar.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="gantiEmail" class="btn btn-blue font-weight-bold py-2">Ya, Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Modal Ganti Password -->
+<div class="modal fade" id="gantiPassword" tabindex="-1" role="dialog" aria-labelledby="gantiPassword" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-bold">Ganti Password</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Password Lama</label>
+                        <input type="text" name="passwordlama" class="form-control" id="exampleFormControlInput1" placeholder="Masukkan password lama">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Password Baru</label>
+                        <input type="text" name="password1" class="form-control" id="exampleFormControlInput1" placeholder="Masukkan password baru">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Konfirmasi Password Baru</label>
+                        <input type="text" name="password2" class="form-control" id="exampleFormControlInput1" placeholder="Masukkan kembali password baru">
+                    </div>
+                    <div class="form-group">
+                        <p>Perhatikan</p>
+                        <ul>
+                            <li>Setelah anda mengklik konfirmasi, anda akan otomatis keluar.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="gantiPassword" class="btn btn-blue font-weight-bold py-3">Ya, Konfirmasi</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
